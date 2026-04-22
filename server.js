e046af3f75a1586b3b02aeab40557900f3265b98
raelis-code <raelisnguessan@gmail.com>
2026-04-23 01:59:09 +0530

    add gitignore

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+const SECRET_KEY = process.env.SECRET_KEY || "default-secret";
+console.log("Secret chargé :", SECRET_KEY);
+
+io.on('connection', socket => {
+    console.log('Utilisateur connecté');
+
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Utilisateur déconnecté');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
