69360f758722476c914c182e902753a1184b8df3
raelis-code <raelisnguessan@gmail.com>
2026-04-23 02:27:09 +0530

    modif

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
