076c995a797211e9a6647cad1f48af102680984c
raelis-code <raelisnguessan@gmail.com>
2026-04-23 02:20:54 +0530

    fix index route

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('public'));
-
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 const SECRET_KEY = process.env.SECRET_KEY || "default-secret";
 console.log("Secret chargé :", SECRET_KEY);
 
