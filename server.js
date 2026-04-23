6d4ac366f64800c88ed0b82d6779d9d3d04ca406
raelis-code <raelisnguessan@gmail.com>
2026-04-23 15:39:16 +0530

    update key

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+
+
 const express = require('express');
 const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+
 app.use(express.static('public'));
+const keyVaultUrl = "https://chatapp-keyvaul.vault.azure.net/";
+
+const credential = new DefaultAzureCredential();
+const client = new SecretClient(keyVaultUrl, credential);
+
+async function getSecret() {
+    const secret = await client.getSecret("secret-key");
+    return secret.value;
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-const SECRET_KEY = process.env.SECRET_KEY || "default-secret";
-console.log("Secret chargé :", SECRET_KEY);
 
 io.on('connection', socket => {
     console.log('Utilisateur connecté');
@@ -25,6 +37,18 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+let SECRET_KEY;
+
+(async () => {
+    try {
+        SECRET_KEY = await getSecret();
+        console.log("Secret récupéré depuis Key Vault :", SECRET_KEY);
+    } catch (error) {
+        console.log("Azure indisponible, fallback local");
+        SECRET_KEY = "local-secret"; 
+    }
+})();
 
 http.listen(PORT, () => {
   console.log(`Serveur lancé sur le port ${PORT}`);
