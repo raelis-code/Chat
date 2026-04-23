6a51c52d67a6dcb85cfaf696fae1c433d1eac904
raelis-code <raelisnguessan@gmail.com>
2026-04-24 02:26:19 +0530

    modif

--- server.js
+++ server.js
@@ -1,69 +1,17 @@
-const appInsights = require("applicationinsights");
-
-appInsights.setup().start();
-
-const clientInsights = appInsights.defaultClient;
-
-const express = require('express');
-const app = express();
-
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-const { DefaultAzureCredential } = require("@azure/identity");
-const { SecretClient } = require("@azure/keyvault-secrets");
+const express = require('express'); const app = express();
+const http = require('http').createServer(app); const io = require('socket.io')(http);
 
 app.use(express.static('public'));
-const keyVaultUrl = "https://chatapp-keyvaul.vault.azure.net/";
 
-const credential = new DefaultAzureCredential();
-const client = new SecretClient(keyVaultUrl, credential);
+io.on('connection', socket => { console.log('A user connected');
 
-async function getSecret() {
-    const secret = await client.getSecret("secret-key");
-    return secret.value;
-}
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+socket.on('chat message', msg => { io.emit('chat message', msg);
 });
 
-io.on('connection', socket => {
-    console.log('Utilisateur connecté');
-
-    clientInsights.trackEvent({ name: "User connected" });
-
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-    });
-
-    socket.on('disconnect', () => {
-        clientInsights.trackEvent({ name: "User disconnected" });
-        console.log('Utilisateur déconnecté');
-        clientInsights.trackEvent({
-  name: "Message received",
-  properties: { message: msg }
-
+socket.on('disconnect', () => { console.log('A user disconnected');
 });
-    });
 });
 
-const PORT = process.env.PORT || 3000;
-
-let SECRET_KEY;
-
-(async () => {
-    try {
-        SECRET_KEY = await getSecret();
-        console.log("Secret récupéré depuis Key Vault :", SECRET_KEY);
-    } catch (error) {
-        console.log("Azure indisponible, fallback local");
-        SECRET_KEY = "local-secret"; 
-    }
-})();
-
-clientInsights.trackEvent({ name: "Server started" });
-
-http.listen(PORT, () => {
-  console.log(`Serveur lancé sur le port ${PORT}`);
+const PORT = 3000; http.listen(PORT, () => {
+console.log(`Server is running on http://localhost:${PORT}`);
 });
